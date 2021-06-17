e90946c434dd471683dc2792027d7fe35e63c940
duwareon <duwareon@gmail.com>
2021-06-17 15:03:49 -0400

    moved some things, changed default size

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
 (require 'org-journal)
 (require 'epa-file)
 
+
 (epa-file-enable)
 (load-theme 'gruvbox-dark-medium t)
 (evil-mode 1)
@@ -37,34 +38,26 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(height . 35))
 (add-to-list 'default-frame-alist '(width . 80))
+
+
+;; Keys
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
+
 (custom-set-variables
+ '(custom-safe-themes
+   '("6f358ac456ee889d132b784afaaa1ce0bd9bdfafc89ef6e3fd5e22179ef59905" default))
+ '(inhibit-startup-screen t)
+ '(org-agenda-files nil)
  '(org-journal-dir "E:/OneDrive/Documents/Emacs/Journal/")
  '(org-journal-encrypt-journal t)
  '(org-journal-enable-encryption t)
  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
  )
-
-;; create the autosave dir if necessary, since emacs won't.
-(make-directory "~/.emacs.d/autosaves/" t)
-
-;; Reminder, do not fuck with this. I know you want to, Trenton.
-;; Don't.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("6f358ac456ee889d132b784afaaa1ce0bd9bdfafc89ef6e3fd5e22179ef59905" default))
- '(inhibit-startup-screen t)
- '(org-agenda-files nil)
- )
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; nil
  )
