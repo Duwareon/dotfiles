9f79da6cd4a215ee95cd7bb81568155dddbaec81
duwareon <duwareon@gmail.com>
2022-07-21 20:42:18 -0400

    continued to add things as well as make minor configs

--- init.el
+++ init.el
@@ -13,37 +13,49 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-
-
+(setq use-package-always-ensure t) 
 ;; basic packages
 (use-package which-key
-  :ensure
   :init
   (which-key-mode))
 
 (use-package selectrum
-  :ensure
   :init
   (selectrum-mode)
   :custom
   (completion-styles '(flex substring partial-completion)))
 
 (use-package gruvbox-theme
-  :ensure
   :init)
 
 (use-package evil
-  :ensure
   :init
   (evil-mode 1))
 
-(use-package magit :ensure)
+(use-package dirvish
+  :init
+  (dirvish-override-dired-mode))
 
+(use-package hyperbole
+  :init
+  (hyperbole-mode 1))
 
+(use-package org)
+
+(use-package mixed-pitch
+  :config
+  
+  (add-hook 'text-mode-hook #'mixed-pitch-mode)
+  (add-hook 'org-mode-hook #'mixed-pitch-mode))
+
+(use-package magit)
+
+(use-package vterm)
+
+(use-package flycheck)
 
 ;; Programming stuff
 (use-package rustic
-  :ensure
   :bind (:map rustic-mode-map
               ("M-j" . lsp-ui-imenu)
               ("M-?" . lsp-find-references)
@@ -80,7 +92,6 @@
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
 (use-package lsp-ui
-  :ensure
   :commands lsp-ui-mode
   :custom
   (lsp-ui-peek-always-show t)
@@ -89,31 +100,81 @@
   
 
 (use-package company
-  :ensure
   :custom
-  (company-idle-delay 0.5) ;; how long to wait until popup
+  (company-idle-delay 0) ;; how long to wait until popup
   ;; (company-begin-commands nil) ;; uncomment to disable popup
   :bind
   (:map company-active-map
 	      ("C-n". company-select-next)
 	      ("C-p". company-select-previous)
 	      ("M-<". company-select-first)
-	      ("M->". company-select-last)))
-
-(use-package flycheck :ensure)
+	      ("M->". company-select-last))
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
 
 ;; Simple configurations
+(add-to-list 'default-frame-alist '(height . 20))
+(add-to-list 'default-frame-alist '(width . 80))
 
 (load-theme 'gruvbox-dark-medium t)
 (setq inhibit-startup-message t
       ring-bell-function 'ignore)
-
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode 0))
 
-(set-face-attribute 'default nil :font "Less Perfect DOS VGA-12")
+(set-face-attribute 'default nil :font "JetBrains Mono-12")
+(set-face-attribute 'variable-pitch nil :font "ETBembo-12")
+
+
+(add-hook 'vterm-mode-hook 'turn-off-evil-mode)
+
+(global-unset-key (kbd "<left>"))
+(global-unset-key (kbd "<right>"))
+(global-unset-key (kbd "<up>"))
+(global-unset-key (kbd "<down>"))
+(global-unset-key (kbd "<C-left>"))
+(global-unset-key (kbd "<C-right>"))
+(global-unset-key (kbd "<C-up>"))
+(global-unset-key (kbd "<C-down>"))
+(global-unset-key (kbd "<M-left>"))
+(global-unset-key (kbd "<M-right>"))
+(global-unset-key (kbd "<M-up>"))
+(global-unset-key (kbd "<M-down>"))
+
+(require 'ob-python)
+(require 'ob-latex)
+
+(setq org-hide-emphasis-markers t)
+(font-lock-add-keywords 'org-mode
+			'(("^ +\\([-*]\\) "
+			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(add-hook 'org-mode-hook '(lambda () ('(mixed-pitch-mode)
+				      (setq line-spacing 0.1
+					    header-line-format " "
+					    left-margin-width 2
+					    right-margin-width 2
+					    internal-border-width 2)
+				      (hl-line-mode -1)
+				      (set-window-buffer nil (current-buffer)))))
+(setq org-startup-indented t
+      org-bullets-bullet-list '(" ") ;; no bullets, needs org-bullets package
+      org-ellipsis "  " ;; folding symbol
+      org-pretty-entities t
+      org-hide-emphasis-markers t
+      ;; show actually italicized text instead of /italicized text/
+      org-agenda-block-separator ""
+      org-fontify-whole-heading-line t
+      org-fontify-done-headline t
+      org-fontify-quote-and-verse-blocks t)
+
+(dolist
+    (buf (list " *Minibuf-0*" " *Minibuf-1*" " *Echo Area 0*" " *Echo Area 1*" "*Quail Completions*"))
+  (when (get-buffer buf)
+    (with-current-buffer buf
+      (setq-local face-remapping-alist '((default (:height (1.0/1.2))))))))
 
 (defun open-init ()
   (interactive)
